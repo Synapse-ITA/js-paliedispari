3b1e1d7efcc8625c342f7a500a616b7c912f6584
Synapse-ITA <puggillovalerio@gmail.com>
2023-11-20 17:56:54 +0100

    first game end

--- js/main.js
+++ js/main.js
@@ -5,20 +5,18 @@
 
 // SCELTA E PROMPT DEI DATI
 
-let PariDispariUtente = prompt("Scegli e scrivi Pari o Dispari");
+let pariDispariUtente = prompt("Scegli e scrivi Pari o Dispari").toLowerCase();
 let numUtente = prompt("Scegli il tuo numero da 1 a 5");
 numUtente = parseInt(numUtente);
 let numCPU = Math.floor(Math.random () * 5) + 1;
 
-
 console.log("Numero della CPU" + " " + numCPU);
 console.log("Numero dell'Utente" + " " + numUtente);
-
 
 // GENERIAMO UN NUMERO RANDOM DA 1 A 5 CON LA FUNZIONE
 
 function cpuNumberGenerator () {
-    let numCPU = Math.floor(Math.random () * 5) + 1;
+    return Math.floor(Math.random () * 5) + 1;
     };
 
 // SOMMIAMO I DUE NUMERI
@@ -28,12 +26,22 @@ console.log("Somma dei numeri" + " " + sommaNumeri);
 
 // STABILIAMO SE LA SOMMA DEI DUE NUMERI E' PARI O DISPARI CON LA FUNZIONE
 
-function pariODispari () {
-    if (sommaNumeri % 2 == 0);
+function pariODispari (somma) {
+    if (somma % 2 === 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
 
+// CHI HA VINTO
+
+let risultato = pariODispari(sommaNumeri);
+if (pariDispariUtente === risultato) {
+    console.log("Hai vinto!");
 } else {
-    
+    console.log("Hai Perso!");
 }
 
 
-// CHI HA VINTO
+
